a34f01c836788ecf372e35992c6a2ae497b84629
analytical <cvgomez@gmail.com>
2021-05-31 16:49:28 -0400

    commit inicial

---
+++ libs/typed.js
@@ -0,0 +1,16 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
+<script>
+const options = {
+  strings: [
+    '<pre>data["químico"]</pre>', '<pre>data["bioquímico"]</pre>', '<pre>data["magíster en estadística"]</pre>'
+  ],
+  typeSpeed: 60,
+  backSpeed: 20,
+  cursorChar: '',
+  loop: true
+};
+
+const typed = new Typed('.typed', options);
+const typed2 = new Typed('.typed-eg', options);
+</script>
